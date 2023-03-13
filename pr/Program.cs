015b95135c011c8d6941d3544729f2a91ccaacc1
Elena <lenkabeloglazova@yandex.ru>
2023-03-13 16:39:16 +0300

    исправление ошибок

--- pr/Program.cs
+++ pr/Program.cs
@@ -1,16 +1,17 @@
 ﻿string[] array1 = new string[5] {"123", "23", "hello", "world", "res"};
 string[] array2 = new string[array1.Length];
+int count = 0;
+
 void SecondArrayWithIF(string[] array1, string[] array2){
 
 
-
-    0 = 0;
     for (int i = 0; i < array1.Length; i++)
     {
         if (array1[i].Length <= 3)
         {
-            array2[count] = array1[i];
             
+             array2[count] = array1[i];
+        i++;
         }
     }
 }
