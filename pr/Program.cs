a6a9291d137c2dfabfefd180ad5b3b03d1b9b7f2
Elena <lenkabeloglazova@yandex.ru>
2023-03-13 16:26:17 +0300

    add string

--- pr/Program.cs
+++ pr/Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array1 = new string[5] {"123", "23", "hello", "world", "res"};
+string[] array2 = new string[array1.Length];
